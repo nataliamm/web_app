4d96e8c8ab55a507550424b4511be3042bbbc871
nataliamm <nataliamyshuk@gmail.com>
2015-09-28 16:10:08 +0300

    Added Heroku.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 import os
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.heroku import Heroku
 
+app = Flask(__name__)
+heroku = Heroku(app)
 
 app = Flask(__name__)
 app.config.from_object('config')
