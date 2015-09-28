485edc732f0cab51c45626cba0f162b9fdaddc5b
nataliamm <nataliamyshuk@gmail.com>
2015-09-28 16:21:22 +0300

    Added SQLAlchemy to config

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
 import os
+from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
